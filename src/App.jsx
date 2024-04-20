3b8c17dd0303736e199ec911c27ce2d9e9ea39a7
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-04-20 15:50:23 +0300

    add router and start location

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,25 @@
-import { Heading } from 'components';
+import { Header } from 'components';
+import { lazy, useEffect } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { getUserInfo } from './service';
+const HomePage = lazy(() => import('pages/Home'));
+const RatesPage = lazy(() => import('pages/Rates'));
 
 export const App = () => {
-  return <Heading title="Hello, world!" />;
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(pos => {
+      const latitude = pos.coords.latitude;
+      const longitude = pos.coords.longitude;
+      getUserInfo({ latitude, longitude });
+    });
+  }, []);
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<HomePage />} />
+        <Route path="/rates" element={<RatesPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Route>
+    </Routes>
+  );
 };
