4bc7f8be1d5d79d9c1cac9a7d132d99f288df30f
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2024-04-20 14:46:33 +0100

    Liubomyr

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,30 @@
 import { Header } from 'components';
 import { lazy, useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { getUserInfo } from './service';
+import { useDispatch } from 'react-redux';
+import { fetchBaseCurrency } from 'reduxState/currency/operations';
+import { setBaseCurrency } from 'reduxState/currency/currencySlice';
 const HomePage = lazy(() => import('pages/Home'));
 const RatesPage = lazy(() => import('pages/Rates'));
 
 export const App = () => {
+  const dispatch = useDispatch();
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(pos => {
-      const latitude = pos.coords.latitude;
-      const longitude = pos.coords.longitude;
-      getUserInfo({ latitude, longitude });
-    });
-  }, []);
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    const success = ({ coords }) => {
+      dispatch(fetchBaseCurrency(coords));
+    };
+    const error = () => {
+      dispatch(setBaseCurrency('USD'));
+    };
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, [dispatch]);
   return (
     <Routes>
       <Route path="/" element={<Header />}>
